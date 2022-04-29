e3d0877d45ec3c40f098824664a9d4a81ce9f1d1
AnatoliKosarev <anatoli.kosarev@gmail.com>
2022-04-29 20:52:14 +0300

    Added test_guest_should_see_login_link with util methods

--- pages/basePage.py
+++ pages/basePage.py
@@ -1,7 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.timeout = timeout
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, locator_type, locator):
+        try:
+            self.browser.find_element(locator_type, locator)
+        except NoSuchElementException:
+            return False
+
+        return True
