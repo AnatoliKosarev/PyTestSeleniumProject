f986a674454b32a1f25cd1e3c687d0fb67ececcd
AnatoliKosarev <anatoli.kosarev@gmail.com>
2022-05-27 19:49:28 +0200

    Code refactored before code review

--- test_product_page.py
+++ test_product_page.py
@@ -15,6 +15,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     product_page = ProductPage(browser, link)
@@ -24,7 +25,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo', ["promo=offer0", "promo=offer1", "promo=offer2", "promo=offer3", "promo=offer4",
                                    "promo=offer5", "promo=offer6",
                                    pytest.param("promo=offer7", marks=pytest.mark.xfail),
@@ -63,6 +64,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.is_disappeared_element(*ProductPageLocators.ADDED_SUCCESSFULLY_TO_CART_MESSAGE)
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     product_page = ProductPage(browser, link)
@@ -88,6 +90,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.is_not_element_present(*ProductPageLocators.ADDED_SUCCESSFULLY_TO_CART_MESSAGE)
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?"
         page = ProductPage(browser, link)
